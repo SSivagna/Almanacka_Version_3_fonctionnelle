abe149caf5b177d09d558aab35cdfbe6b7e63717
Sivagna Sinthuja <sivagnanasunda@intechinfo.fr>
2012-12-10 15:39:13 +0100

    commit du CreatePlanning

---
+++ Almanacka Essai/src/com/mysql/CreatePlanning.java
@@ -0,0 +1,49 @@
+package com.mysql;
+
+import java.sql.Connection;
+//import java.sql.Date;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import com.almanacka.planning.InputLesson;
+import com.almanacka.planning.InputPlanning;
+
+
+public class CreatePlanning 
+{
+	static public InputPlanning createPlanning(Connection connection) throws SQLException, ParseException
+	{
+		Statement statement = connection.createStatement();
+		ResultSet rSet = statement.executeQuery("SELECT * FROM almanacka.lesson;");
+
+		ArrayList<InputLesson> lessons = new ArrayList<InputLesson>();
+		HashSet<String> monitorIntensities = new HashSet<String>();
+		
+		while(rSet.next())
+		{
+			String lessonIdFromDB = rSet.getString("lessonId");
+			Boolean isLocked = rSet.getBoolean("block");
+			String idPlaceFromDB = rSet.getString("idPlace");
+		//	Date begDate = rSet.getDate("BegDate");
+		//	Date endDate = rSet.getDate("EndDate");
+			InputLesson b = new InputLesson(lessonIdFromDB, isLocked, idPlaceFromDB/*, new java.util.Date( begDate.getTime() ), new java.util.Date( endDate.getTime() )*/ );
+			
+			lessons.add(b);
+			
+			System.out.println("Données depuis CP : " + lessonIdFromDB +"," /*+ begDate + "," + endDate + "."*/);
+		}
+		
+		rSet = statement.executeQuery("SELECT * FROM almanacka.usermonitorintensity;");
+		while(rSet.next())
+		{
+			String monitorIdFromDB = rSet.getString("idMonitor");
+			String intensityIdFromDB = rSet.getString("idIntensity");			
+			monitorIntensities.add(intensityIdFromDB + '|' + monitorIdFromDB);
+		}
+		return new InputPlanning(lessons, monitorIntensities);
+	}
+}
